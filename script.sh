a92db8420d556519653c712991580b0bd4acb740
Archie Linux <archie.linux@tech.com>
2025-10-06 02:28:45 +0530

    install chrome driver

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install google-chrome-stable
+
+version=$(echo google-chrome --version)
+
+# install dependencies
+sudo apt install -y unzip xvfb libxi6 libgconf-2-4
+
+wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
+
+unzip chromedriver_linux64.zip
+
+sudo mv chromedriver /usr/local/bin/
+
+sudo chmod +x /usr/local/bin/chromedriver
+
